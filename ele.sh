4f44ca7c32990c77918fec888fbacb3ec5160288
Qingyi Pan <pqy_edu@163.com>
2020-05-29 20:34:34 +0800

    Update ele.sh

--- ele.sh
+++ ele.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python main.py --horizon 6 --data data/electricity.txt --save save/elec.pt --output_fun Linear --model LSTNet --skip 24 --batch_size 32 --hidCNN 50 --hidRNN 50
+python main.py --horizon 6 --data data/electricity.txt --save save/elec.pt --output_fun Linear --model CNN --skip 24 --batch_size 32 --hidCNN 50 --hidRNN 50
